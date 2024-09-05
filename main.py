283b372cce0db7a5b7e6669c840049e8daedc1ec
Ton Paes <ton.paes.bccs@gmail.com>
2024-09-05 09:10:30 -0300

    docstring for tests

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+"""
+Main app
+"""
 from fastapi import FastAPI, HTTPException
 from service import count_vowels
 from models import VowelCountRequest, SortRequest
+
 
 app = FastAPI()
 
 @app.post("/vowel_count")
 async def vowel_count(request: VowelCountRequest):
+    """
+    Count the number of vowels in a given word.
+
+    Args:
+    word (str): The word to count vowels in.
+
+    Returns:
+    int: The number of vowels in the word.
+    """
     word_counts = {word: count_vowels(word) for word in request.words}
     return word_counts
 
 @app.post("/sort")
 async def sort_words(request: SortRequest):
+    """
+    Sort list of words
+
+    Args:
+    List of words to count.
+
+    Returns:
+    list of words in specified words.
+    """
     if request.order not in {"asc", "desc"}:
         raise HTTPException(status_code=400, detail="Invalid order parameter")
     sorted_words = sorted(request.words, reverse=request.order == "desc")
--- test_main.py
+++ test_main.py
@@ -1,24 +1,31 @@
+"""
+Tests
+"""
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
 def test_vowel_count():
+    """Verify vowel counts for given words at /vowel_count."""
     response = client.post("/vowel_count", json={"words": ["batman", "robin", "coringa"]})
     assert response.status_code == 200
     assert response.json() == {"batman": 2, "robin": 2, "coringa": 3}
 
 def test_sort_asc():
+    """Check sorting in ascending order at /sort."""
     response = client.post("/sort", json={"words": ["batman", "robin", "coringa"], "order": "asc"})
     assert response.status_code == 200
     assert response.json() == ["batman", "coringa", "robin"]
 
 def test_sort_desc():
+    """Check sorting in descending order at /sort."""
     response = client.post("/sort", json={"words": ["batman", "robin", "coringa"], "order": "desc"})
     assert response.status_code == 200
     assert response.json() == ["robin", "coringa", "batman"]
 
 def test_sort_invalid_order():
-    response = client.post("/sort", json={"words": ["batman", "robin", "coringa"], "order": "invalid"})
+    """Verify error handling for invalid order at /sort."""
+    response = client.post("/sort", json={"words": ["batman", "robin"], "order": "invalid"})
     assert response.status_code == 400
     assert response.json() == {"detail": "Invalid order parameter"}
